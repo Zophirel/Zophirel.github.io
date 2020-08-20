c5b98c11693336bb4148c7c757b2fc01798b75cd
Zophirel <sworld.design@gmail.com>
2020-08-20 14:46:42 +0200

    fixed animation and mobile

--- js/columnResize.js
+++ js/columnResize.js
@@ -1,18 +1,27 @@
-grid = document.getElementsByClassName('grid-container');
-firstItem = grid[0].children[0];
-secondItem = grid[0].children[1];
-thirdItem = grid[0].children[2];
+grid = document.getElementsByClassName('grid-container')[0];
+firstItem = grid.children[0];
+secondItem = grid.children[1];
+thirdItem = grid.children[2];
 
-const itemTouch = (item) => {
-    grid[0].style.transition = 'grid-template-columns 0.5s ease';
-    if(item == firstItem)
-        grid[0].style.gridTemplateColumns = '0.25fr 0.15fr 0.15fr';
-    if(item == secondItem)
-        grid[0].style.gridTemplateColumns = '0.15fr 0.25fr 0.15fr';
-    if(item == thirdItem)
-        grid[0].style.gridTemplateColumns = '0.15fr 0.15fr 0.25fr';
+if(window.matchMedia("(min-width: 768px)").matches){
+    var itemTouch = (item) => {
+        grid.style.transition = 'all 0.5s ease';
+        if(item == firstItem)
+            grid.style.gridTemplateColumns = '0.25fr 0.15fr 0.15fr';
+        if(item == secondItem)
+            grid.style.gridTemplateColumns = '0.15fr 0.25fr 0.15fr';
+        if(item == thirdItem)
+            grid.style.gridTemplateColumns = '0.15fr 0.15fr 0.25fr';
+    }
+
+    var standardGrid = () => {
+        grid.style.gridTemplateColumns = '0.2fr 0.2fr 0.2fr';
+    }
 }
-
-const standardGrid = () => {
-    grid[0].style.gridTemplateColumns = '0.2fr 0.2fr 0.2fr';
+else{
+    const items = [firstItem, secondItem, thirdItem];
+    for (let i = 0; i < items.length; i++){
+        items[i].removeAttribute("onmouseenter");
+        items[i].removeAttribute("onmouseleave");
+    }
 }
